8f3a1e94c23e452a8479abd0a9cf155d3550eb10
ZhouYong <zhyong319@163.com>
2018-02-08 22:22:14 +0800

    init version1.0

---
+++ RandLuckMan/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace RandLuckMan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.OnTimeUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("楷体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(167, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 53);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Section";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("黑体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(167, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(131, 53);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(13, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "random";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(632, 363);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "抽奖软件 Copyright 2018 by zy";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ RandLuckMan/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace RandLuckMan
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument xmlDocument;
+        XmlNodeList xmlNodeList;
+        int keyPressCount;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            keyPressCount = 0;
+            
+                LoadXML("UserInfoLib.xml"); //加载XML文件
+            if (File.Exists("background.jpg")) //如果有背景图片，加载背景图片
+            {
+                try
+                 {
+                    this.BackgroundImage = Image.FromFile("background.jpg");
+                }
+
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("没有找到指定的 userInfoLib.xml 文件，请检查");
+                }
+            }
+           
+
+        }
+        
+        private int GetRandNumber(int upNum) //获取随机数
+        {
+            int randNum;
+            DateTime dateTime = DateTime.Now;
+            Random random = new Random((int)dateTime.Ticks);
+            randNum = random.Next(upNum);
+            return randNum;
+        }
+
+        private void LoadXML(string filePath) //加载XML文件
+        {
+            xmlDocument = new XmlDocument();
+            try
+            {
+                xmlDocument.Load(filePath);
+            }
+            catch (XmlException)
+            {
+                MessageBox.Show("在 XML 中没有加载或解析错误.");
+                return;
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("UserInfoLib.XML文档没有找到,请检查并确认文件名");
+                return;
+            }
+            XmlElement xmlElement = xmlDocument.DocumentElement;
+            xmlNodeList = xmlElement.ChildNodes;
+        }
+
+        private void SelectLuckUser() //随机选择选中的用户
+        {
+            int num = 0;          
+            num = GetRandNumber(xmlNodeList.Count);
+            string strSection, strName;
+            XmlNodeList childNodeList = xmlNodeList.Item(num).ChildNodes;
+            strSection = childNodeList.Item(0).InnerText;
+            strName = childNodeList.Item(1).InnerText;
+            this.label1.Text = strSection;
+            this.label2.Text = strName;
+            this.label3.Text = num.ToString();
+        }
+        private void OnKeyPress(object sender, KeyEventArgs e)   //检查什么键按下了
+        {
+           
+            if (e.KeyCode == Keys.Space) //按下空格
+            {
+                if (keyPressCount == 0)
+                {
+                    this.timer1.Enabled = true;
+                    keyPressCount++;
+                }
+                else
+                {
+                    this.timer1.Enabled = false;
+                    keyPressCount = 0;
+                }
+            }
+            if (e.KeyCode == Keys.Q) //按下Q键
+            {
+                this.Close();
+            }
+            if (e.KeyCode == Keys.H) //按下H键
+            {
+
+            }
+        }
+
+        private void OnTimeUp(object sender, EventArgs e)
+        {
+            SelectLuckUser();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
