e75df68eefd9644328dac9a41b1a348cd6ce4e23
XRH_GCB_PC <>
2018-03-04 23:51:55 +0800

    “You can click the label(1,2) move anywhere,and adjust label3 at left button at app start

--- RandLuckMan/Form1.Designer.cs
+++ RandLuckMan/Form1.Designer.cs
@@ -42,47 +42,53 @@
             // 
             // label1
             // 
+            this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("楷体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label1.ForeColor = System.Drawing.Color.Transparent;
-            this.label1.Location = new System.Drawing.Point(148, 69);
+            this.label1.Location = new System.Drawing.Point(127, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(241, 99);
+            this.label1.Size = new System.Drawing.Size(188, 48);
             this.label1.TabIndex = 0;
             this.label1.Text = "Section";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDownLabel1);
             // 
             // label2
             // 
+            this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("黑体", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label2.ForeColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(148, 179);
+            this.label2.Location = new System.Drawing.Point(127, 165);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(241, 83);
+            this.label2.Size = new System.Drawing.Size(116, 48);
             this.label2.TabIndex = 1;
             this.label2.Text = "Name";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.OnMouseDownLabel2);
             // 
             // label3
             // 
+            this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("宋体", 14.26415F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label3.ForeColor = System.Drawing.Color.Transparent;
-            this.label3.Location = new System.Drawing.Point(13, 337);
+            this.label3.Location = new System.Drawing.Point(11, 311);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(76, 21);
+            this.label3.Size = new System.Drawing.Size(69, 20);
             this.label3.TabIndex = 2;
             this.label3.Text = "random";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.SandyBrown;
-            this.ClientSize = new System.Drawing.Size(632, 363);
+            this.ClientSize = new System.Drawing.Size(542, 335);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -92,7 +98,9 @@
             this.Text = "抽奖软件 Copyright 2018 by zy";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyPress);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.OnMouseMove);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
--- RandLuckMan/Form1.cs
+++ RandLuckMan/Form1.cs
@@ -17,6 +17,10 @@ namespace RandLuckMan
         XmlDocument xmlDocument;
         XmlNodeList xmlNodeList;
         int keyPressCount;
+        bool configModeFlag;
+        int mouseClickCouter; //鼠标单击次数计数
+        Label selectLable; // 保存选中的控件
+        
         public struct LUCKMANINFO
         {
             public string section;
@@ -26,7 +30,10 @@ namespace RandLuckMan
         public Form1()
         {
             InitializeComponent();
-            
+            mouseClickCouter = 0;
+            configModeFlag = false;
+           
+            selectLable = null;
             keyPressCount = 0;
             this.WindowState = FormWindowState.Maximized; //全屏显示
             this.FormBorderStyle = FormBorderStyle.None;
@@ -55,6 +62,7 @@ namespace RandLuckMan
                 }
             }
 
+            this.label3.Location = new Point(0,Screen.GetBounds(this).Bottom-30); //设置LABEL3控件的位置
 
         }
 
@@ -111,21 +119,38 @@ namespace RandLuckMan
                 {
                     this.timer1.Enabled = true;
                     keyPressCount++;
+                    return;
                 }
                 else
                 {
                     this.timer1.Enabled = false;
                     keyPressCount = 0;
                     Write(luckManInfo);
+                    return;
                 }
             }
             if (e.KeyCode == Keys.Q) //按下Q键
             {
                 this.Close();
+                return;
             }
             if (e.KeyCode == Keys.H) //按下H键
             {
                 // Insert code here
+                return;
+            }
+            if (e.KeyCode == Keys.S) //按下S键
+            {
+                configModeFlag = true; //进入配置模式
+                this.label3.Text = "进入配置模式";
+                return;
+            }
+            if (e.KeyCode == Keys.Escape) //按下ESC键
+            {
+                configModeFlag = false;
+                selectLable = null;
+                this.label3.Text = "退出配置模式";
+                return;
             }
         }
 
@@ -136,7 +161,21 @@ namespace RandLuckMan
 
         private void label1_Click(object sender, EventArgs e)
         {
+            if (configModeFlag == true)
+            {
+                
+                selectLable = this.label1;
+                
+            }
+        }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+            if (configModeFlag == true)
+            {
+               
+                selectLable = this.label2;
+            }
         }
 
         private void Write(LUCKMANINFO man)
@@ -150,6 +189,37 @@ namespace RandLuckMan
                 sw.WriteLine("------------------------------------------------------------------");
             }        
 
+        }
+
+        private void OnMouseDownLabel1(object sender, MouseEventArgs e)  //label控件1上鼠标单击
+        {
+           
+            mouseClickCouter++;
+            if (mouseClickCouter >=2)
+            {
+                selectLable = null;
+                mouseClickCouter = 0;
+            }
+            this.label3.Text = "Mouse Click Down. " + mouseClickCouter.ToString() + " times!";
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (configModeFlag == true)
+            {
+                if(mouseClickCouter ==1 )  selectLable.Location = new Point(e.X, e.Y);
+            }
+        }
+
+        private void OnMouseDownLabel2(object sender, MouseEventArgs e) //labe2控件1上鼠标单击
+        {
+            mouseClickCouter++;
+            if (mouseClickCouter >= 2)
+            {
+                selectLable = null;
+                mouseClickCouter = 0;
+            }
+            this.label3.Text = "Mouse Click Down. " + mouseClickCouter.ToString() + " times!";
         }
     }
 }
